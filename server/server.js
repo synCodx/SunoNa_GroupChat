3d0f600ace9d1ca1459e4797399fe960aab419bd
Ankit Barik <137585959+synCodx@users.noreply.github.com>
2025-09-20 21:01:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://suno-na-group-chat.vercel.app/',
     },
 });
 
